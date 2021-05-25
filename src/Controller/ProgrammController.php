21245dd492a779915c47a9b3e1d6899ecb0439ce
Thierry-68 <thierry.sandre68@gmail.com>
2021-05-25 11:00:08 +0200

    05 Symfony

--- src/Controller/ProgrammController.php
+++ src/Controller/ProgrammController.php
@@ -6,13 +6,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/programs", name="program_")
+ */
 class ProgrammController extends AbstractController
 {
     /**
-     * @Route("/programs", name="programs_index")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
         return new Response('<html><body>The new Wild Series Index</body><hml>');
     } 
+
+     /**
+     * @Route("/{id}", methods={"GET"}, requirements={"id"="\d+"},  name="show")
+     */
+    public function show(int $id) : Response
+    {
+        return $this->render('program/show.html.twig', ['id' => $id]);
+    }
+
 }
